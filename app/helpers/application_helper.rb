197f44691beba01c841e200acb142872526885aa
benalavi <benalavi@gmail.com>
2009-04-25 17:13:48 -0700

    added about page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+  # Uses the given url as the title of the link and links to it (so you don't
+  # have to repeat the URL in your view)
+  def link_to_url(url)
+    link_to url, url
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
-  map.resources :images
   map.root :controller => 'images', :action => 'index'
+  map.resources :images
+  %w( about ).each do |page|
+    map.send("#{page}_page", "/#{page}", :controller => 'pages', :action => 'show', :page => page)
+  end
 end
